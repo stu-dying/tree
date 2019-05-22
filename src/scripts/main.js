1d19b4b983cb00281bc161f59b7ba3b79752ea9f
valeriitovstyk <valeriitovstyk@gmail.com>
2019-05-22 06:49:40 +0300

    solution for create_tree by @laverka

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('hello mate academy!');
+const container = document.getElementById('container');
+createTree(container, {
+  'Рыбы': {
+    'Форель': {},
+    'Щука': {},
+  },
+
+  'Деревья': {
+    'Хвойные': {
+      'Лиственница': {},
+      'Ель': {},
+    },
+    'Цветковые': {
+      'Берёза': {},
+      'Тополь': {},
+    },
+  },
+});
+
+function createTree(container, data) {
+  const ul = document.createElement('ul');
+  container.append(ul);
+  for (const tree in data) {
+    const li = document.createElement('li');
+    li.innerHTML = tree;
+    ul.append(li);
+    if (Object.keys(data[tree]).length > 0) {
+      createTree(li, data[tree]);
+    }
+  }
+}
+
